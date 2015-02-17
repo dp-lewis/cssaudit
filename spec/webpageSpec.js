8e24c952fe926d57165abf77173d1d48d34f014a
David Lewis <mail@david-lewis.com>
2015-02-17 20:33:05 +1100

    add means to call pages in a series

--- spec/webpageSpec.js
+++ spec/webpageSpec.js
@@ -2,9 +2,11 @@
 
 var webpage = require('../lib/webpage.js');
 
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 60000000;
+
 describe('webpage', function () {
   it('should do its shit', function (done) {
-    webpage.testPage().then(function () {
+    webpage.testPages(['http://www.google.com.au', 'http://www.finder.com.au']).then(function () {
       done();
     });
   });
