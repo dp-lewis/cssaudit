edcedaa24435de592aba2fd2501c8caf1ad450b7
David Lewis <mail@david-lewis.com>
2015-02-19 08:04:56 +1100

    fixed looping problem

--- spec/sitemapSpec.js
+++ spec/sitemapSpec.js
@@ -1,9 +1,17 @@
+/*global describe, it, expect*/
 var sitemap = require('../lib/sitemap.js');
 
 
 describe('sitemap', function () {
-  it('should do its shit', function (done) {
+  it('should do fetch a sitemap', function (done) {
     sitemap.fetch('http://www.finder.com.au/sitemap.xml').then(function (result) {
+      expect(typeof result).toBe('object');
+      done();
+    });
+  });
+
+  it('should do fetch multiple sitemaps', function (done) {
+    sitemap.fetch(['http://www.finder.com.au/sitemap.xml', 'http://www.finder.com.au/sitemap.xml']).then(function (result) {
       expect(typeof result).toBe('object');
       done();
     });
--- spec/webpageSpec.js
+++ spec/webpageSpec.js
@@ -2,13 +2,13 @@
 
 var webpage = require('../lib/webpage.js');
 
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 60000000;
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 600000;
 
 describe('webpage', function () {
   it('should do its shit', function (done) {
     var pagesToTest, testToRun;
 
-    pagesToTest = ['http://www.google.com.au', 'http://www.finder.com.au'];
+    pagesToTest = ['http://www.google.com.au'];
 
     testToRun = function () {
       return document.title;
