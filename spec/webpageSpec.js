49f756847c3ea7cc6eabcc5f7fa230295ce160dd
David Lewis <mail@david-lewis.com>
2015-02-20 07:56:39 +1100

    Run webpage test in parallel

--- spec/webpageSpec.js
+++ spec/webpageSpec.js
@@ -8,13 +8,14 @@ describe('webpage', function () {
   it('should do its shit', function (done) {
     var pagesToTest, testToRun;
 
-    pagesToTest = ['http://www.google.com.au'];
+    pagesToTest = ['http://www.google.com.au', 'http://www.finder.com.au'];
 
     testToRun = function () {
       return document.title;
     };
 
     webpage.testPages(pagesToTest, testToRun).then(function (results) {
+      console.log(results)
       expect(typeof results).toBe('object');
       done();
     });
