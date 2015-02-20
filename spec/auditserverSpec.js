7d422f04f586b34c110aa212a4f34041fd78ae96
David Lewis <mail@david-lewis.com>
2015-02-21 10:09:34 +1100

    Add async testing to webpage module

--- spec/auditserverSpec.js
+++ spec/auditserverSpec.js
@@ -4,7 +4,6 @@ var auditserver = require('../lib/auditserver.js');
 describe('audit server', function () {
   it('should do its shit', function (done) {
     auditserver.start(['html', 'body']).then(function (server) {
-      console.log(server);
       expect(typeof server).toBe('object');
       server.stop();
       done();
--- spec/webpageSpec.js
+++ spec/webpageSpec.js
@@ -1,4 +1,4 @@
-/*global expect, describe, it, jasmine*/
+/*global window, expect, describe, it, jasmine*/
 
 var webpage = require('../lib/webpage.js');
 
@@ -6,17 +6,45 @@ jasmine.DEFAULT_TIMEOUT_INTERVAL = 600000;
 
 describe('webpage', function () {
   it('should do its shit', function (done) {
-    var pagesToTest, testToRun;
+    var pagesToTest, testToRun, collectResult;
 
     pagesToTest = ['http://www.google.com.au', 'http://www.finder.com.au'];
 
     testToRun = function () {
-      return document.title;
+      window.didItWork = 'it certainly did';
     };
 
-    webpage.testPages(pagesToTest, testToRun).then(function (results) {
-      console.log(results)
+    collectResult = function () {
+      return window.didItWork;
+    };
+
+    webpage.testPages(pagesToTest, testToRun, collectResult).then(function (results) {
       expect(typeof results).toBe('object');
+      expect(results.length).toBe(2);
+      expect(results[0]).toBe('it certainly did');
+      done();
+    });
+  });
+
+  it('should do its shit with an asynchronous test', function (done) {
+    var pagesToTest, testToRun, collectResult;
+
+    pagesToTest = ['http://www.google.com.au'];
+
+    testToRun = function () {
+      window.setTimeout(function () {
+        window.didItWork = 'hell yeah!';
+      }, 500);
+
+    };
+
+    collectResult = function () {
+      return window.didItWork || false;
+    };
+
+    webpage.testPages(pagesToTest, testToRun, collectResult).then(function (results) {
+      expect(typeof results).toBe('object');
+      expect(results[0]).toBe('hell yeah!');
       done();
     });
   });
