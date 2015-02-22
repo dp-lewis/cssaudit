77ad4a41bdc42a2909e81ea4934bf33cb0ed5866
David Lewis <mail@david-lewis.com>
2015-02-22 20:55:35 +1100

    Update readme and example

--- README.md
+++ README.md
@@ -2,3 +2,21 @@
 
 # cssaudit
 CSS Audit helps identify unused selectors in your CSS
+
+
+```
+var cssaudit = require('./lib/cssaudit'),
+  myaudit;
+
+myaudit = cssaudit.init({
+  'stylesheets': ['http://www.david-lewis.com/wp-content/themes/davidlewis/style.css'],
+  'sitemaps': ['http://www.david-lewis.com/sitemap-posttype-post.xml'],
+  'output': './output/david-lewis.html'
+});
+
+myaudit.run().done(function () {
+  console.log('done');
+}, function (err) {
+  console.log(err);
+});
+```
--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 var cssaudit = require('./lib/cssaudit'),
-  stylesheets,
-  sitemaps,
   myaudit;
 
-stylesheets = ['http://www.david-lewis.com/wp-content/themes/davidlewis/style.css'];
-sitemaps = ['http://www.david-lewis.com/sitemap-posttype-post.xml'];
-
 myaudit = cssaudit.init({
-  'stylesheets': stylesheets,
-  'sitemaps': sitemaps,
-  'output': './output/dpl.html'
+  'stylesheets': ['http://www.david-lewis.com/wp-content/themes/davidlewis/style.css'],
+  'sitemaps': ['http://www.david-lewis.com/sitemap-posttype-post.xml'],
+  'output': './output/david-lewis.html'
 });
 
 myaudit.run().done(function () {
