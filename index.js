5c91e1df6f6bc9cd1f1b5da13033063ed9648410
David Lewis <mail@david-lewis.com>
2015-02-23 10:36:10 +1100

    Add customisable template option

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ myaudit = cssaudit.init({
   'stylesheets': ['http://www.david-lewis.com/wp-content/themes/davidlewis/style.css'],
   'sitemaps': ['http://www.david-lewis.com/sitemap-posttype-post.xml'],
   'output': './output/david-lewis',
-  'filename': 'myreport'
+  'filename': 'myreport',
+  'template': './output/template.mustache'
 });
 
 myaudit.run().done(function () {
