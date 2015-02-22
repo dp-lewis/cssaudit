3ef228bcf37e3c7a04c16434f418897ba743e7c1
David Lewis <mail@david-lewis.com>
2015-02-23 06:50:44 +1100

    Add nock to fetchurl spec

--- spec/fetchurlSpec.js
+++ spec/fetchurlSpec.js
@@ -1,10 +1,24 @@
-var fetchurl = require('../lib/fetchurl.js');
+/*global it, describe, expect */
+var fetchurl = require('../lib/fetchurl.js'),
+  fs = require('fs'),
+  nock = require('nock'),
+  nockSitemaps;
+
+nockSitemaps = nock('http://localhost', {
+  reqheaders: {
+    'Content-Type': 'text/xml'
+  }
+}).get('/sitemap1.xml')
+  .times(1)
+  .reply(200, function () {
+    return fs.createReadStream(__dirname + '/fixtures/sitemap1.xml');
+  });
 
 
 describe('fetch url', function () {
-  it('should do its shit', function (done) {
-    fetchurl.fetch('http://www.david-lewis.com/sitemap-posttype-post.xml').then(function (result) {
-	  expect(typeof result).toBe('string');  	
+  it('should get the contents of a url', function (done) {
+    fetchurl.fetch('http://localhost/sitemap1.xml').then(function (result) {
+      expect(typeof result).toBe('string');
       done();
     });
   });
