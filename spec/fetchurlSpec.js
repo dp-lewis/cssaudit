aea8e56d1ca6b5cca2731d6c1b7f6db6399168e5
David Lewis <mail@david-lewis.com>
2015-02-22 19:22:23 +1100

    Use david lewis

--- spec/fetchurlSpec.js
+++ spec/fetchurlSpec.js
@@ -3,7 +3,7 @@ var fetchurl = require('../lib/fetchurl.js');
 
 describe('fetch url', function () {
   it('should do its shit', function (done) {
-    fetchurl.fetch('http://www.finder.com.au/sitemap.xml').then(function (result) {
+    fetchurl.fetch('http://www.david-lewis.com/sitemap-posttype-post.xml').then(function (result) {
 	  expect(typeof result).toBe('string');  	
       done();
     });
