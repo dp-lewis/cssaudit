402f6afa8139d391e6dacfc1cd4a2a94f32f3f13
David Lewis <mail@david-lewis.com>
2015-02-23 06:37:54 +1100

    Use nock for http mocking on sitemap spec

--- spec/sitemapSpec.js
+++ spec/sitemapSpec.js
@@ -1,18 +1,37 @@
 /*global describe, it, expect*/
-var sitemap = require('../lib/sitemap.js');
+var sitemap = require('../lib/sitemap.js'),
+  fs = require('fs'),
+  nock = require('nock'),
+  nockSitemaps;
 
+nockSitemaps = nock('http://localhost', {
+  reqheaders: {
+    'Content-Type': 'text/xml'
+  }
+}).get('/sitemap1.xml')
+  .times(2)
+  .reply(200, function () {
+    return fs.createReadStream(__dirname + '/fixtures/sitemap1.xml');
+  })
+  .get('/sitemap2.xml')
+  .times(1)
+  .reply(200, function () {
+    return fs.createReadStream(__dirname + '/fixtures/sitemap2.xml');
+  });
 
 describe('sitemap', function () {
   it('should do fetch a sitemap', function (done) {
-    sitemap.fetch('http://www.finder.com.au/sitemap.xml').then(function (result) {
+    sitemap.fetch('http://localhost/sitemap1.xml').then(function (result) {
       expect(typeof result).toBe('object');
+      expect(result.length).toBe(1);
       done();
     });
   });
 
-  it('should do fetch multiple sitemaps', function (done) {
-    sitemap.fetch(['http://www.finder.com.au/sitemap.xml', 'http://www.finder.com.au/sitemap.xml']).then(function (result) {
+  it('should fetch multiple sitemaps', function (done) {
+    sitemap.fetch(['http://localhost/sitemap1.xml', 'http://localhost/sitemap2.xml']).then(function (result) {
       expect(typeof result).toBe('object');
+      expect(result.length).toBe(2);
       done();
     });
   });
